658e584667931eaab3341fca57b02811a8ed78f6
Jcg408 <jcg321@comcas.net>
2019-02-25 15:09:43 -0500

    project file structure done

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,14 @@
 class ArtistsController < ApplicationController
+
+  get '/artists' do
+    @artists = Artist.all
+    erb :'/artists/index'
+  end
+
+  get '/artists/:slug' do
+    @artist = Artist.find {|artist| artist.parameterize == params[:slug]}
+
+
+    erb :'/artists/show'
+  end
 end
