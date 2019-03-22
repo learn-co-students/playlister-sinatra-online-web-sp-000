68b7a97e1fe69a23898ab782f2a4116341a591a4
Anup Khadka <akhadka.dev@gmail.com>
2019-03-22 22:19:21 +0000

    Done.

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -8,8 +8,9 @@ module Slugifiable
 
   module ClassMethods
     def find_by_slug(slug)
-      name = slug.gsub("-"," ").titleize
-      self.find_by(name: name)
+      name = slug.gsub("-"," ").downcase
+      song = self.all.find {|song| song.name.downcase == name}
+      song
     end
   end
 end
