896a36e291a1f568346c1bba6df586ac3bfd2320
Diana Badas <diana.badas@gmail.com>
2019-04-12 09:28:56 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_12_091121) do
+ActiveRecord::Schema.define(version: 2019_04_12_092554) do
+
+  create_table "artist_genres", force: :cascade do |t|
+    t.integer "artist_id"
+    t.integer "genre_id"
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "song_genres", force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "genre_id"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "title"
+  end
 
 end
