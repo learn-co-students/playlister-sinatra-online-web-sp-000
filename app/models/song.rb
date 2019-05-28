6aa42b10bc12114dc8b86c2291c0b949b30cc54b
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-28 17:39:02 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,13 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :genres, through: :song_genres
+  attr_accessor :genre_ids
+
+  @genre_ids = []
+
+  def genre_ids
+    self.genres.each do |genre|
+      self.genre_ids << genre.id
+    end
+  end
 end
---
+++ db/migrate/20190528164919_add_artist_id_column_to_genres.rb
@@ -0,0 +1,5 @@
+class AddArtistIdColumnToGenres < ActiveRecord::Migration[5.2]
+  def change
+    add_column :genres, :artist_id, :integer
+  end
+end
