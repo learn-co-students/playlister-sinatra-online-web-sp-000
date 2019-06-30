e195909ea37a8859d1f845d9b1ea445910720456
Bry Cox <carl.cc@gmail.com>
2019-06-30 23:44:34 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,15 @@
 class SongsController < ApplicationController
 
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :"songs/new"
+  end
+
+  get '/songs' do
+    @songs = Song.all
+    erb :"songs/index"
+  end
+
 
 
 end
