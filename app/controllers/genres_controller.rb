807cf3e07d581a07d99ef181ad6db8d0c573455f
Bry Cox <carl.cc@gmail.com>
2019-07-02 03:23:48 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,5 +4,11 @@ class GenresController < ApplicationController
     @genres = Genre.all
     erb :"genres/index"
   end
-  
+
+  get '/genres/:slug' do
+    slug = params[:slug]
+    @genre = Genre.find_by_slug(slug)
+    erb :"genres/show"
+  end
+
 end
