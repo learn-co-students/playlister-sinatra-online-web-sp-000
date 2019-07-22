bed3b43c4bcdca5e10732d9ce3e2b9437c2b57e9
Alan Krajina <alankrajina88@gmail.com>
2019-07-22 10:53:58 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,14 +15,36 @@ class SongsController < ApplicationController
     erb :'/songs/new'
   end
 
+# stavljanje svega iz NEW u attr accessore @song
   post '/songs' do
-  @song = Song.create(:name => params["name"])
+  @song = Song.create(:name => params["song_name"])
   @artist = Artist.find_or_create_by(:name => params["artist_name"])
 
+  @song.artist = @artist
+  @song.genre_ids = params[:genre_ids]
 
+  @song.save
 
-
-  redirect "/songs/:slug"
+#  flash[:message] = "Successfully created song."
+  redirect "/songs/#{@song.slug}"
   end
+
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
+  end
+
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+
+
+
+
+#    redirect "/owners/#{@owner.id}"
+  end
+
 
 end
