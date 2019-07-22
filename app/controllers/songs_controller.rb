af7c4af96e5e03775709d90a2f28c18240d03112
Alan Krajina <alankrajina88@gmail.com>
2019-07-22 13:12:50 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,22 +6,21 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
-  get '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    erb :'/songs/show'
-  end
-
   get '/songs/new' do
     erb :'/songs/new'
   end
 
-# stavljanje svega iz NEW u attr accessore @song
   post '/songs' do
-  @song = Song.create(:name => params["song_name"])
+    binding.pry
+  @song = Song.new(:name => params["song_name"])
   @artist = Artist.find_or_create_by(:name => params["artist_name"])
 
   @song.artist = @artist
-  @song.genre_ids = params[:genre_ids]
+  @song.genre_ids = params[:genre_ids]  #should be 1
+
+#  params["genre_ids"].each do |id|
+#    @song.genres << Genre.find(id)
+#  end
 
   @song.save
 
@@ -29,6 +28,13 @@ class SongsController < ApplicationController
   redirect "/songs/#{@song.slug}"
   end
 
+
+
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
+  end
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,14 +2,14 @@ class Artist < ActiveRecord::Base
 
   has_many :songs
   has_many :genres, through: :songs
-  
+
   def slug
     self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
   end
-  
+
   def self.find_by_slug(slug)
     Artist.all.find do |artist|
-    artist.slug == slug      
+    artist.slug == slug
     end
   end
 end
