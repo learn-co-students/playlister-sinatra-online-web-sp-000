19b04a65884d3325702c10d1d453b0f3c7ce37f6
Ramon Chavez <rachavez1974@gmail.com>
2019-07-24 23:02:24 -0400

    Finish basic spec show page for artist

--- spec/features/04_basic_view_spec.rb
+++ spec/features/04_basic_view_spec.rb
@@ -99,31 +99,31 @@ describe "Playlister Basics" do
       end
     end
 
-    # describe "/artists/:slug" do
-    #   before do
-    #     visit "/artists/#{@artist.slug}"
-    #   end
+    describe "/artists/:slug" do
+      before do
+        visit "/artists/#{@artist.slug}"
+      end
 
-    #   it 'responds with a 200 status code' do
-    #     expect(page.status_code).to eq(200)
-    #   end
+      it 'responds with a 200 status code' do
+        expect(page.status_code).to eq(200)
+      end
 
-    #   it "displays the artist's songs" do
-    #     expect(page).to have_content(song_name)
-    #   end
+      it "displays the artist's songs" do
+        expect(page).to have_content(song_name)
+      end
 
-    #   it "displays the artist's genres" do
-    #     expect(page).to have_content(genre_name)
-    #   end
+      it "displays the artist's genres" do
+        expect(page).to have_content(genre_name)
+      end
 
-    #   it "contains links to each song's show page" do
-    #     expect(page).to have_css("a[href='/songs/#{@song.slug}']")
-    #   end
+      it "contains links to each song's show page" do
+        expect(page).to have_css("a[href='/songs/#{@song.slug}']")
+      end
 
-    #   it "contains links to each genre's show page" do
-    #     expect(page).to have_css("a[href='/genres/#{@genre.slug}']")
-    #   end
-    # end
+      it "contains links to each genre's show page" do
+        expect(page).to have_css("a[href='/genres/#{@genre.slug}']")
+      end
+    end
 
     # describe "/genres/:slug" do
     #   before do
