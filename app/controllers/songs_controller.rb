5d83ea5c0d8f804aacb325e26aadf6eef68fcb2b
Jared Grove <jaredkgrove@gmail.com>
2019-07-25 20:49:56 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,30 @@
+
+
+
 class SongsController < ApplicationController
+use Rack::Flash
   get '/songs' do
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
+    flash[:message] = "Successfully created song."
     erb :'songs/show'
+  end
+
+  post '/songs' do
+    @song = Song.create(params[:song])
+
+    if !params[:artist_name].empty?
+      @artist = Artist.find_or_create_by(name:params[:artist_name])
+      @song.update(artist:@artist)
+    end
+
+    redirect "/songs/#{@song.slug}"
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 ENV['SINATRA_ENV'] ||= "development"
 
+require 'rack-flash'
 require 'bundler/setup'
+
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 def fi_check_migration
