260d4e7481cb653e553f6ab1f234d0825730f76e
Jared Grove <jaredkgrove@gmail.com>
2019-07-25 21:29:48 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,8 +13,21 @@ use Rack::Flash
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-
     erb :'songs/show'
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @artist = Artist.find_or_create_by(name:params[:artist_name])
+    @song.update(artist:@artist)
+    flash[:message]="Successfully updated song."
+    redirect :"songs/#{@song.slug}"
   end
 
   post '/songs' do
