319ac6a47ed28074ca41c14dbd152681df002a49
Ramon Chavez <rachavez1974@gmail.com>
2019-07-28 03:09:37 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,9 +24,33 @@ class SongsController < ApplicationController
           SongGenre.create(song_id: @song.id, genre_id: genre_id)
         end
     end
-
     flash[:message] = "Successfully created song."
     redirect to "/songs/#{@song.slug.downcase}"
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    @genres = Genre.all
+    erb :'/songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    
+    @song = Song.find_by_slug(params[:slug])
+    @artist = @song.artist
+     if params["Artist Name"] != @artist.name
+      @song.artist.update_attribute(:name, params["Artist Name"])
+      if !((params["genre_names"].length == @song.genres.length) && (params["genre_names"].sort == @song.genres.sort))
+         @song.update_genres(params["genre_names"])
+       end
+     end
+    
+    if !((params["genre_names"].length == @song.genres.length) && (params["genre_names"].sort == @song.genres.sort))
+      @song.update_genres(params["genre_names"])
+    end
+  
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
   end
 
   get '/songs' do
--- spec/features/05_song_form_spec.rb
+++ spec/features/05_song_form_spec.rb
@@ -78,61 +78,61 @@ describe "Song Forms" do
         expect(page.current_path).to eq('/songs/that-one-with-the-guitar')
       end
     end
-  # end
+  end
 
-  # describe "/songs/:slug/edit" do
-  #   before do
-  #     @song = Song.create(name: song_name)
-  #     artist = Artist.create(name: artist_name)
+  describe "/songs/:slug/edit" do
+    before do
+      @song = Song.create(name: song_name)
+      artist = Artist.create(name: artist_name)
 
-  #     @song.song_genres.create(genre: genre_1)
-  #     @song.artist = artist
+      @song.song_genres.create(genre: genre_1)
+      @song.artist = artist
 
-  #     @song.save
+      @song.save
 
-  #     visit "/songs/#{@song.slug}/edit"
-  #   end
+      visit "/songs/#{@song.slug}/edit"
+    end
 
-    # context "changing a song's artist" do
-    #   it "updates the song's artist" do
-    #     fill_in "Artist Name", with: "Some Nobody"
-    #     click_on "Save"
+    context "changing a song's artist" do
+      it "updates the song's artist" do
+        fill_in "Artist Name", with: "Some Nobody"
+        click_on "Save"
 
-    #     expect(page).to have_content("Successfully updated song.")
-    #     expect(page).to have_content(song_name)
-    #     expect(page).to have_content("Some Nobody")
-    #   end
+        expect(page).to have_content("Successfully updated song.")
+        expect(page).to have_content(song_name)
+        expect(page).to have_content("Some Nobody")
+      end
 
-    #   it "renders to the song show page" do
-    #     fill_in "Artist Name", with: "That singer"
-    #     click_on "Save"
-    #     expect(page.current_path).to eq("/songs/that-one-with-the-guitar")
-    #   end
-    # end
+      it "renders to the song show page" do
+        fill_in "Artist Name", with: "That singer"
+        click_on "Save"
+        expect(page.current_path).to eq("/songs/that-one-with-the-guitar")
+      end
+    end
 
-    # context "changing a song's genres" do
-    #   it "has a checkbox element on the form" do
-    #     expect(page.body).to include("checkbox")
-    #   end
+    context "changing a song's genres" do
+      it "has a checkbox element on the form" do
+        expect(page.body).to include("checkbox")
+      end
 
-    #   it "updates the song's genres" do
-    #     uncheck "New Age Garbage"
-    #     check "Hippity Hop"
-    #     click_on "Save"
+      it "updates the song's genres" do
+        uncheck "New Age Garbage"
+        check "Hippity Hop"
+        click_on "Save"
 
-    #     expect(page).to have_content("Successfully updated song.")
-    #     expect(page).to have_content(song_name)
-    #     expect(page).to have_content(artist_name)
-    #     expect(page).to have_content("Hippity Hop")
-    #     expect(page).not_to have_content("New Age Garbage")
-    #   end
+        expect(page).to have_content("Successfully updated song.")
+        expect(page).to have_content(song_name)
+        expect(page).to have_content(artist_name)
+        expect(page).to have_content("Hippity Hop")
+        expect(page).not_to have_content("New Age Garbage")
+      end
 
-      # it "renders to the song show page" do
-      #   uncheck "Hippity Hop"
-      #   check "New Age Garbage"
-      #   click_on "Save"
-      #   expect(page.current_path).to eq("/songs/that-one-with-the-guitar")
-      # end
-    # end
+      it "renders to the song show page" do
+        uncheck "Hippity Hop"
+        check "New Age Garbage"
+        click_on "Save"
+        expect(page.current_path).to eq("/songs/that-one-with-the-guitar")
+      end
+    end
   end
 end
