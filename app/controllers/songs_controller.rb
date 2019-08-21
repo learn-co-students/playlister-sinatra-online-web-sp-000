2908d4d4edd04797474a2fa4869f9d0e6156c065
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-08-21 23:27:58 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,45 +2,43 @@ require 'rack-flash'
 class SongsController < ApplicationController
   enable :sessions
   use Rack::Flash
-  
+
   get '/songs' do
     @songs = Song.all
     erb :'/songs/index'
   end
-  
+
   post '/songs' do
-    @song = Song.create(params[:name])
-    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
-    @song.genre = params[:genres]
+    @song = Song.create(params[:song])
+    @song.artist = Artist.find_or_create_by(:name => params[:song][:artist_id])
     @song.save
-    
     flash[:message] = "Successfully created song."
     redirect "/songs/#{@song.slug}"
   end
-  
+
   get '/songs/new' do
     @songs = Song.all
-    @genres = Genre.all
     erb :'/songs/new'
   end
-  
+
   get '/songs/:slug/edit' do
-    @song = Song.find(params[:slug])
-    @genres = Genre.all
-    erb:'songs/edit'
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
   end
-  
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
   end
-  
-  post '/songs/:slug' do
+
+  patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
-    @song.update(params["song"])
-    
-    flash[:message] = "Succesfully updated song." 
+    #binding.pry
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(:name => params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save
+    flash[:message] = "Succesfully updated song."
     redirect "/songs/#{@song.slug}"
   end
 end
--- config.ru
+++ config.ru
@@ -4,8 +4,17 @@ begin
   fi_check_migration
 
   use Rack::MethodOverride
+  use ArtistsController
+  use GenresController
+  use SongsController
   run ApplicationController
 rescue ActiveRecord::PendingMigrationError => err
   STDERR.puts err
   exit 1
 end
+
+#use Rack::MethodOverride
+#use ArtistsController
+#use GenresController
+#use SongsController
+#run ApplicationController
