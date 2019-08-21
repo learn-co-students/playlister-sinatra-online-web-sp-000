344ca360f735a262f11249ccab82854aa17be953
Yingqi Chen <chanwk18@hotmail.com>
2019-08-21 15:59:30 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,21 @@
+class SongsController < ApplicationController
+
+  get '/songs' do
+    @songs=Song.all
+    erb :"songs/index"
+  end
+
+  get '/songs/new' do
+    erb :"/songs/new"
+  end
+
+  patch '/songs/:slug' do
+    erb :"songs/edit"
+  end
+
+  get '/songs/:slug' do
+    @song= Song.find_by_slug(params[:slug])
+    erb :"/songs/show"
+  end
+
+end
