9c26c9bbaa68600e1e40325ff6974c39a0d32311
juan siu <juan.siu@gmail.com>
2019-09-26 16:38:57 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,16 +17,23 @@ class SongsController < ApplicationController
   end
   
   post "/songs" do 
+    if params["song"]["name"] == ""
+      "Please write the name of the Song"
+       redirect to "/songs"
+     end
     @song = Song.create(params[:song])
-        if !params["artist"]["name"].empty?
-      @song.artist = Artist.create(name: params["artist"]["name"])
-      @song.save
-        elsif @artist = Artist.find(params["artist"]["name"])
-      @song.artist = @artist
-      @song.save
+      if !params["artist"]["name"].empty?
+        @song.artist = Artist.create(name: params["artist"]["name"])
+        @song.save
+      elsif params["artist"]["name"] == ""
+        @song.artist = nil
+        @song.save
+      elsif  @artist = Artist.find(params["artist"]["name"])
+        @song.artist = @artist
+        @song.save
     end
     #flash[:message] = "Successfully created song."
-    redirect "songs/#{@song.slug}"
+    redirect to "songs/#{@song.slug}"
   end
   
   get "/songs/:slug" do 
@@ -45,11 +52,17 @@ class SongsController < ApplicationController
     if !params[:song].keys.include?("artist_id")
     params[:song]["artist_id"] = []
     end
-    @song = Song.find(params[:slug])
+    @song = Song.find_by_slug(params[:slug])
     @song.update(params["song"])
-    if !params["artist"]["name"].empty?
-      @song.artist = Artist.create(name: params["artist"]["name"])
-      @song.save
+        if !params["artist"]["name"].empty?
+        @song.artist = Artist.create(name: params["artist"]["name"])
+        @song.save
+      elsif params["artist"]["name"] == ""
+        @song.artist = nil
+        @song.save
+      elsif  @artist = Artist.find(params["artist"]["name"])
+        @song.artist = @artist
+        @song.save
     end
     redirect to "/songs"
   end
