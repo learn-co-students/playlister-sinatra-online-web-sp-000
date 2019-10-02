cca6411f8db0bd8fdc00ca7cc0186290802eba79
Alice Brunel <alc.brunel@gmail.com>
2019-10-02 13:05:23 +0000

    Done.

--- app/models/artists.rb
+++ app/models/artists.rb
@@ -1,7 +1,9 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-  has_many :genres, through: :songs
-  
+  has_many :genres, :through => :songs
+
+# We are repeating a ton of code here. These two methods could go into a module.
+
   def slug 
     slug_name = self.name.downcase.split(" ")
     slug_name.join("-")
