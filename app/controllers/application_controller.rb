04a65f3f26debfb181452821738f122a32e4e4ed
Alice Brunel <alc.brunel@gmail.com>
2019-10-02 18:16:00 +0100

    create a controller for each class, added the Rack::MethodOverride in config.ru

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,36 +6,8 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
-    
-  get '/songs' do  
-	@songs = Song.all
-	erb :'song/songs'
-  end
-
-  get '/artists' do
-    @artists = Artist.all
-	erb :'artist/artists'
-  end
   
-  get '/genres' do 
-	@genres = Genre.all 
-	erb :'genre/genres'
-	end 
 
-  get '/songs/:slug' do 
-	 @song = Song.find_by_slug(params[:slug])
-    erb :'song/show'
-  end
-
-  get '/artists/:slug' do 
-	 @artist = Artist.find_by_slug(params[:slug])
-    erb :'artist/show'
-  end
-
-  get '/genres/:slug' do 
-	 @genre = Genre.find_by_slug(params[:slug])
-    erb :'genre/show'
-  end
 
 
 end
