20cdd1379ad0ed84ecd5df586805d9793ef64258
Alice Brunel <alc.brunel@gmail.com>
2019-10-02 12:05:57 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+  
+  def slug 
+    slug_name = self.name.downcase.split(" ")
+    slug_name.join("-")
+  end
+
+  def self.find_by_slug(slug_name)
+    Song.all.find{|song| song.slug == slug_name}
+  end
+  
 end
