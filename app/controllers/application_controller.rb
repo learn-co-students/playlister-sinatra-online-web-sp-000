07066414ef999da4dcb3a5c28adc27bf54855197
Frederic Silber <freddy.silber@cloudcoach.com>
2019-10-07 23:44:45 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
-# require 'rack-flash'
-# require 'sinatra/base'
+require 'rack-flash'
+require 'sinatra/base'
 # class ApplicationController < Sinatra::Base
 #   register Sinatra::ActiveRecordExtension
 #   set :session_secret, "my_application_secret"
@@ -9,10 +9,20 @@
 #     erb :index
 #   end
 # end
+# class ApplicationController < Sinatra::Base
+#   register Sinatra::ActiveRecordExtension
+#
+#   enable :sessions
+#   set :session_secret, "my_application_secret"
+#   set :views, Proc.new { File.join(root, "../views/") }
+#
+#   get '/' do
+#     erb :index
+#   end
+# end
+
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-
-  enable :sessions
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
