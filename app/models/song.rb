c1e47fcf8530060d7e308273cd6b478d5d8d24ce
Lauren McCoy <lmccoy8911@gmail.com>
2019-10-15 20:15:31 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,20 @@ class Song < ActiveRecord::Base
   has_many :song_genres
   has_many :genres, through: :song_genres
   belongs_to :artist 
+  
+  def slug 
+    @name = name.downcase
+    @name.split.join("-")
+  end 
+  
+  def self.find_by_slug(title)
+    @songs = Song.all 
+    new_song = nil 
+    
+    @songs.each do |song|
+      song.slug == title ? new_song = song : nil 
+    end 
+    new_song
+  end 
+  
 end 
