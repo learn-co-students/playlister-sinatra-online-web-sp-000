1c277e37ca25195f9f39220510843fdb6311f952
Lauren McCoy <lm_lutec@outlook.com>
2019-10-16 10:36:55 -0400

    displayed the artist's songs

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,8 +7,7 @@ class ArtistsController < ApplicationController
   end 
 
   get '/artists/:slug' do 
-    @artist = Artist.find_by_slug(params[:slug])
-    binding.pry 
+    @artist = Artist.find_by_slug(params[:slug]) 
     erb :'/artists/show'
   end 
 
