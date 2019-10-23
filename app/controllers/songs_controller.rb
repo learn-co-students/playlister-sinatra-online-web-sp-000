9910e225959387277fcf05476a21b51f350b4397
Mark Daniel  Delgado <markpnoi26@gmail.com>
2019-10-23 00:33:14 +0000

    songs/new edits

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,11 +5,23 @@ class SongsController < ApplicationController
     erb :"songs/index"
   end
 
+  get '/songs/new' do
+    erb :"songs/new"
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     @genres = @song.genres
     @artist = @song.artist
     erb :"songs/show"
+  end
+
+  post '/songs' do
+    #create a song with existing artist and existing genre
+    #create a song without existing artist and existing genre
+    #cant leave the radio button blank
+    #cant leave the genre checkbox blank
+    puts params
   end
 
 end
