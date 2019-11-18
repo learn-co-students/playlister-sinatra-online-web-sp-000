7e347997121a111929a8956d3b930d20aaf058fe
Lindsey Wells <github email address>
2019-11-18 19:54:10 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,4 @@
+require "pry"
 class SongsController < ApplicationController
 
     get '/songs' do
@@ -5,9 +6,27 @@ class SongsController < ApplicationController
         erb :'/songs/index'
       end
 
+      get '/songs/new' do
+        @genres=Genre.all
+        erb :'/songs/new'
+      end
+
       get '/songs/:slug' do
         @song=Song.find_by_slug(params[:slug])
         erb :'/songs/show'
       end
+
+      post '/songs' do
+        @song=Song.create(name: params[:song][:name])
+        @song.artist=Artist.find_or_create_by(name: params[:artist][:name])
+        @song.genre_ids = params[:genres]
+        @song.save 
+        
+        redirect to "/songs/#{@song.slug}"
+      end
+
+
+
+      
 
 end
