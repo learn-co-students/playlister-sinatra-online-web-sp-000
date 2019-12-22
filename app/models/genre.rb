e214991ae81b7b077742ceb4a154e0466f5114fe
Christopher Dent <christopherdent01@gmail.com>
2019-12-22 15:38:00 -0500

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,7 @@
-class Artist < ActiveRecord::Base
- 
+class Genre < ActiveRecord::Base
+has_many  :song_genres
+ has_many  :songs, through: :song_genres
+ has_many  :artists, through: :songs
+ include Slugifiable::SlugMethod
+ extend Slugifiable::FindBySlug
 end
--- db/migrate/20191221043829_create_song_genre.rb
+++ db/migrate/20191221043829_create_song_genre.rb
@@ -1,6 +1,6 @@
 class CreateSongGenre < ActiveRecord::Migration[5.2]
   def change
-     drop_table :genres do |t|
+     create_table :song_genres do |t|
       t.integer :song_id
       t.integer :genre_id
     end
