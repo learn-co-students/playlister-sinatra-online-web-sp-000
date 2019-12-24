53305800d1d4875f327fc7042ee8040e3d7723b3
Christopher Dent <christopherdent01@gmail.com>
2019-12-23 20:26:11 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,31 +20,28 @@ class SongsController < ApplicationController
     end
 
 
-   post '/songs' do
-    # binding.pry
-       @genre = Genre.all
-       @song = Song.create(:name => params[:song_name])
-       #@song.update(params[:song_name])
-       @song.artist = Artist.find_or_create_by(:name => params[:artist_name])
-       @song.genre = params[:genre_name]
-       if !params[:artist_name].empty?
-          @song.artist = Artist.create(:name => params[:artist_name])
-       end
-       @song.save
-       flash[:message] = "Successfully created song."
-       redirect("/songs/#{@song.slug}")
+    post '/songs' do
+      @song = Song.create(name: params["Name"])
+      @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+      @song.genre_ids = params["genres"]
+      @song.save
+      flash[:message] = "Successfully created song."
+      redirect to("/songs/#{@song.slug}")
     end
 
+    get '/songs/:slug/edit' do
+      @song = Song.find_by_slug(params[:slug])
+      erb :'/songs/edit'
+    end
 
-
-
-    #get '/songs/:slug/edit' do
-    #  @song = Song.find_by_slug(params[:slug])
-    #  erb :'songs/edit'
-    #end
-
-     #patch '/songs/:slug' do
-
+    patch '/songs/:slug' do
+      @song = Song.find_by_slug(params[:slug])
+      @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+      @song.genre_ids = params["genres"]
+      @song.save
+      flash[:message] = "Successfully updated song."
+      redirect to("/songs/#{@song.slug}")
+    end
 
 
 
