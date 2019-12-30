a8f5c989b5eba759e340c9ed88464027b446d96d
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2019-12-30 07:27:17 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 ENV['SINATRA_ENV'] ||= "development"
-
 require 'bundler/setup'
+require_relative 'gremlins'
+
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 def fi_check_migration
---
+++ gremlins.rb
@@ -0,0 +1,14 @@
+module Gremlins
+  module InstanceMethods
+    def slug
+      name.gsub(" ", "-").downcase
+    end
+  end
+
+  module ClassMethods
+    def find_by_slug(slug)
+      self.all.find {|object| object.slug == slug}
+    end
+  end
+
+end
