9fd2dfa659c96c5a41ecfcd2b2fce0ef06d83b4a
BlythePollard <github email address>
2019-12-30 20:27:21 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,12 @@
 class SongsController < ApplicationController
 
     get '/songs' do
-        #get all songs as variable
-        #display songs in erb with clickable link to song's show page
         @songs = Song.all 
         erb :'songs/index'
     end
 
     get '/songs/:slug' do 
+        erb :'songs/show'
         #song's show page has links to song's artist and genre associated with song
         #Pay attention to order here with order compared to /songs/new--slug must be defined first
     end
