cd78c7c2e54696a1b87530157257e401606ebfb1
BlythePollard <github email address>
2019-12-31 20:25:43 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,6 @@
+
+#require 'rack-flash'
+
 class SongsController < ApplicationController
 
     get '/songs' do
@@ -5,12 +8,32 @@ class SongsController < ApplicationController
         erb :'songs/index'
     end
 
+    get '/songs/new' do
+        #if artist exists, add song, if artist is new, add song and artist
+        erb :'songs/new'
+    end 
+
     get '/songs/:slug' do 
         @song = Song.find_by_slug(params[:slug])
+        
         erb :'songs/show'
         #song's show page has links to song's artist and genre associated with song
         #Pay attention to order here with order compared to /songs/new--slug must be defined first
     end
 
+    post '/songs' do
+        @song = Song.create(params[:song])     
+        @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+        @song.genre_ids = params[:genres]
+        @song.save 
+        #this is having error! flash[:message] = "Successfully created song." 
+        redirect("/songs/#{@song.slug}")
+      end
+
+    get '/songs/:slug/edit' do 
+        erb :'songs/edit'
+    end
     #will also have new and show (posts?)
 end
+
+
