aeaa90171696945f99157e393c3ac6470aa92c9e
h-miiaah <haim.miiaah5@gmail.com>
2020-01-22 20:36:29 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,5 @@
 require 'rack-flash'
-
+# require 'pry'
 class SongsController < ApplicationController
   use Rack::Flash
 
@@ -19,11 +19,11 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
+    binding.pry
     @song = Song.create(params[:song])     
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.genre_ids = params[:genres]
     @song.save 
-
     flash[:message] = "Successfully created song." 
     redirect("/songs/#{@song.slug}")
   end
--- config.ru
+++ config.ru
@@ -4,11 +4,15 @@ begin
   fi_check_migration
 
   use Rack::MethodOverride
-  use SongsController
-  use ArtistsController
-  use GenresController
+ 
   run ApplicationController
 rescue ActiveRecord::PendingMigrationError => err
   STDERR.puts err
   exit 1
 end
+
+use Rack::MethodOverride
+use SongsController
+use ArtistsController
+use GenresController
+run ApplicationController
