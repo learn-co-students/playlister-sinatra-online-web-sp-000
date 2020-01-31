15cfef4246d9fc47db16915b715ce83fbcc08131
Mike White <michaeljwhite@gmail.com>
2020-01-30 23:48:59 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,6 @@
+require 'rack-flash'
 class SongsController < ApplicationController
+    use Rack::Flash
 
     get '/songs' do
         @songs = Song.all.sort_by { |song| song.name }
@@ -11,13 +13,11 @@ class SongsController < ApplicationController
     end
 
     post '/songs' do
-        #binding.pry
         artist = Artist.find_or_create_by_name(name: params[:artist][:name])
         song = Song.find_or_create_by_name(name: params[:song][:name])
         if params[:genres]
             genres = []
             params[:genres].each do |name|
-            #binding.pry
             genre = Genre.find_or_create_by_name(name: name)
             genres << genre unless genres.include?(genre)
           end
@@ -31,14 +31,14 @@ class SongsController < ApplicationController
         song.artist = artist
         song.genres = genres
         song.save
-        # flash Successfully created song.
+        flash[:message] = "Successfully created song."
         redirect to "/songs/#{song.slug}"
     end
     
     get '/songs/:slug/edit' do
         @song = Song.all.find_by_slug(params[:slug])
         @artist = @song.artist
-        @genres = @song.genres
+        @genres = Genre.all
         erb :'songs/edit'
     end
 
@@ -61,13 +61,14 @@ class SongsController < ApplicationController
         end
 
         if params[:genre][:name] != ""
+            genres ||= []
             genres << Genre.find_or_create_by_name(name: params[:genre][:name])
         end
 
         song.artist = artist
         song.genres = genres
         song.save
-        # flash Successfully updated song.
+        flash[:message] = "Successfully updated song."
         redirect to "/songs/#{song.slug}"
     end    
 
