13da20249877a6c361423de75503f5ae8c7d528b
Chris Cheong <chrisxcheong@gmail.com>
2020-02-05 04:41:05 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,4 @@
 class SongsController <ApplicationController
-  get '/' do
-    @songs = Song.all
-    erb :'/songs/index'
-  end
 
   get '/songs' do
     @songs = Song.all
@@ -13,14 +9,25 @@ class SongsController <ApplicationController
     erb :new
   end
 
-  get '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-  erb :show
-  end
 
-  get '/songs/:id' do
-    @song = Song.find_by_id(params[:id])
+  get '/songs/:slug' do
+    @song = Song.find_by_id(params[:slug])
     erb :'/songs/show'
   end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :edit
+  end
+
+  post '/songs' do
+    song = Song.create(name: params[:song][:name])
+    artist = Artist.find_or_create_by(name: params[:song][:artist])
+    song.genre_ids = params[:genres]
+    song.artist = artist
+    song.save
+    redirect to("/songs/#{song.slug}")
+end
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 # Add seed data here. Seed your database with `rake db:seed`
-require './config/environment'
+
 LibraryParser.parse
