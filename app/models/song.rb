50d1b896172eefa38e36df6c56b48f2e68863088
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-02-05 02:46:00 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,7 @@
+class Song < ActiveRecord::Base
+  extend Slugification::ForClass
+  include Slugification::ForInstance
+  belongs_to :artist
+  has_many :song_genre_binds
+  has_many :genres, through: :song_genre_binds
+end
