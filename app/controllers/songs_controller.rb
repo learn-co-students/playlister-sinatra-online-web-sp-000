a199db9e1204a4837c3532ac2613f968dbd1daa7
Anthony Tilelli <anthony@tilelli.me>
2020-02-16 02:10:18 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,8 @@
-class ArtistsController < ApplicationController
+class SongsController < ApplicationController
  # rake db:migrate SINATRA_ENV=test
  # rspec spec/features/04_basic_view_spec.rb --fail-fast
+  
+ get '/songs' do
+ end
   
 end
