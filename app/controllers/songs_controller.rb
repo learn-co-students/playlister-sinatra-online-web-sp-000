5fa34010b9290677bd8f5de38c617c697e76f701
Anthony Tilelli <anthony@tilelli.me>
2020-02-16 02:42:02 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,5 +4,21 @@ class SongsController < ApplicationController
   
  get '/songs' do
  end
+ 
+ post '/songs' do
+ end
+  
+ 
+  get '/songs/new' do
+  end
+  
+  get '/songs/:slug' do
+  end
+  
+  get '/songs/:slug/edit' do
+  end
+  
+  patch '/songs/:slug' do
+  end
   
 end
