dd283d68594fde405eec2e85d39f195ba84cbb75
Anthony Tilelli <anthony@tilelli.me>
2020-02-17 04:34:54 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,14 +1,14 @@
 class SongsController < ApplicationController
- # rake db:migrate SINATRA_ENV=test
- # rspec spec/features/04_basic_view_spec.rb --fail-fast
+  # rake db:migrate SINATRA_ENV=test
+  # rspec spec/features/04_basic_view_spec.rb --fail-fast
+  get '/songs' do
+    @all_songs = Song.all
+    erb :song_index
+  end
   
- get '/songs' do
- end
- 
- post '/songs' do
- end
+  post '/songs' do
+  end
   
- 
   get '/songs/new' do
   end
   
