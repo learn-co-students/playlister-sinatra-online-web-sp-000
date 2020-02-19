c212e27ec94c28c7d7cb730a5b09c82f1248c0d4
Anthony Tilelli <anthony@tilelli.me>
2020-02-19 03:48:51 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,7 @@
 class SongsController < ApplicationController
   # rake db:migrate ; rake db:migrate SINATRA_ENV=test
   # rspec spec/features/04_basic_view_spec.rb --fail-fast
+  # rspec spec/features/05_song_form_spec.rb --fail-fast
  
   get '/songs' do
     @all_songs = Song.all
@@ -11,6 +12,7 @@ class SongsController < ApplicationController
   end
   
   get '/songs/new' do
+    erb :new_song
   end
   
   get '/songs/:slug' do
@@ -20,6 +22,9 @@ class SongsController < ApplicationController
   end
   
   get '/songs/:slug/edit' do
+    @slug = params[:slug]
+    @slug_song = Song.find_by_slug(@slug)
+    erb :songs_slug_edit
   end
   
   patch '/songs/:slug' do
