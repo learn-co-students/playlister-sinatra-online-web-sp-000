50081e0dbace43b67168627c6bfa8646ef628276
Will Carter <will.carter@fergusllc.com>
2020-03-09 12:16:29 -0600

    Done.

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -7,21 +7,7 @@ module Slugifiable
     
     module ClassMethods
         def find_by_slug(slug)
-            str = slug.gsub("-", " ")
-            self.all.find_by(:name => titleize(str))
-        end
-
-        def titleize(str)
-            str.capitalize!  # capitalize the first word in case it is part of the no words array
-            words_no_cap = ["and", "or", "the", "over", "to", "the", "a", "but", "with"]
-            phrase = str.split(" ").map {|word| 
-                if words_no_cap.include?(word) 
-                    word
-                else
-                    word.capitalize
-                end
-            }.join(" ") # I replaced the "end" in "end.join(" ") with "}" because it wasn't working in Ruby 2.1.1
-          phrase  # returns the phrase with all the excluded words
+            self.all.find{ |instance| instance.slug == slug }
         end
     end
 end
