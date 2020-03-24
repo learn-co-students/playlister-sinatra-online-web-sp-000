c3ad5922e1a37834ab38a013629d9f87b14214d4
CoderJay06 <jaycru06@yahoo.com>
2020-03-24 18:19:03 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,30 @@ class ApplicationController < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
+    
     erb :index
   end
+
+  get '/songs' do 
+    # presents the user with list of all songs
+    # as clickable links to that song's show page
+  end 
+
+  get '/genres' do 
+    # presents the user with a list of all genres 
+    # as clickable links to that genre's show page
+  end 
+
+  get '/artists' do 
+    # presents the user with a list of all artists 
+    # as clickable links to that song's show page 
+  end 
+
+
 end
+
+
+
+
+
+
