e18281123d45849d371fa0898a7dae1f2f7781b5
CoderJay06 <jaycru06@yahoo.com>
2020-03-27 23:04:44 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
-@jay_z = Artst.new(name: "Jay-Z")
-@99_problems = Song.new(name: "99 Problems", artist: @jay_z)
+@jay_z = Artist.new(name: "Jay-Z")
+@problems = Song.new(name: "99 Problems", artist: @jay_z)
 @hip_hop = Genre.new(name: "Hip Hop")
 
 @mobb_deep = Artist.new(name: "Mobb Deep")
 @infamous = Song.new(name: "Infamous", artist: @mobb_deep)
 @rap = Genre.new(name: "Rap")
 
+@bob_marely = Artist.new(name: "Bob Marely")
+@dont_worry = Song.new(name: "Don't Worry About A Thing")
+@regge = Genre.new(name: "Regge")
