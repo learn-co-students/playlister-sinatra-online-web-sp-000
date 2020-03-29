afc690dca8c308f95d8a495613ba2d5576fa7b63
CoderJay06 <jaycru06@yahoo.com>
2020-03-29 03:29:50 -0400

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,11 +3,15 @@ class GenresController < ApplicationController
    get '/genres' do 
       # presents the user with a list of all genres 
       # as clickable links to that genre's show page
+      @genres = Genre.all 
+      erb :'/genres/index'
    end 
 
    get '/genres/:slug' do 
       # genre's show page should have links to each of 
       # its artists and songs.
+      @genre = Genre.find_by_slug(param[:slug])
+      erb :'/genres/show'
    end 
 
 end 
