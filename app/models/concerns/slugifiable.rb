c74020c2720f8e3913723c70e8d792b3aaf4f92e
CoderJay06 <jaycru06@yahoo.com>
2020-03-28 20:45:27 -0400

    Done.

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -2,15 +2,15 @@ module Slugifiable
    module InstanceMethods
       def slug 
          #binding.pry
-         self.name.downcase.parameterize
+         self.name.parameterize
       end 
    end 
 
    module ClassMethods  
    
       def find_by_slug(slug)
-         binding.pry 
-         Artist.all.detect{|artist| artist.name == slug}
+        artist_name = slug.split('-').collect{|n| n.capitalize}.join(" ")
+        Artist.all.find_by(name: artist_name)
       end 
    end 
 end 
