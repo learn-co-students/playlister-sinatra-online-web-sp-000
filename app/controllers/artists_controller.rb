831af8f965ec5775263423c52f900f18d1266ffe
CoderJay06 <jaycru06@yahoo.com>
2020-03-28 23:27:02 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,6 @@
 class ArtistsController < ApplicationController
 
    get '/artists' do 
-      #raise params.inspect
       # presents the user with a list of all artists 
       # as clickable links to that song's show page
       #binding.pry
@@ -13,10 +12,6 @@ class ArtistsController < ApplicationController
       # artist's show page should have links to each 
       # of his or her songs and genres.
       @artist = Artist.find_by_slug(params[:slug])
-      erb :"/artists/show"
-   end 
-
-   post '/artists/' do 
-      @artist = Artist.new(name: params[:name])
+      erb :'/artists/show'
    end 
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,11 +9,22 @@ class SongsController < ApplicationController
    ## Pay attention to the order of /songs/new and /songs/:slug. 
    ## The route /songs/new could interpret new as a slug if that 
    ## controller action isn't defined first.
+   get '/songs/new' do 
+
+      erb :'/songs/new'
+   end 
 
    get '/songs/:slug' do 
       # song's show page should have links to that 
       # song's artist and the each genre associated 
       # with the song.
+      @song = Song.find_by_slug(params[:slug])
+      erb :'/songs/show'
+   end 
+
+   post '/songs/new' do 
+      @song = Song.create(name: params["name"])
+      erb :'/songs/:slug'
    end 
 
 end 
