32dd36cf7b9ae8502b9e519c3f88235e8bf4471f
doakcovington <doakcovington@protonmail.com>
2020-04-29 13:27:36 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,6 @@
-class ApplicationController < Sinatra::Base
-  register Sinatra::ActiveRecordExtension
 
-  enable :sessions
-  set :session_secret, "my_application_secret"
-  set :views, Proc.new { File.join(root, "../views/") }
 
-  get '/' do
-    erb :index
-  end
+class SongsController < ApplicationController
 
   get '/songs' do
     @songs = Song.all
