e8fdb561581e22995fc237d852f5f89121e106d5
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-01 16:14:42 -0400

    songs show page working

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,12 @@ class SongsController < ApplicationController
 
   get '/songs' do
     @all_songs = Song.all
-    erb :songs_index
+    erb :'songs/index'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
   end
 
 end
