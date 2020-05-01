7c698e114d28218656ca322ceba8a75b00e6e40d
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-01 16:18:33 -0400

    artists show page working

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,7 +2,12 @@ class ArtistsController < ApplicationController
 
   get '/artists' do
     @all_artists = Artist.all
-    erb :artists_index
+    erb :'artists/index'
+  end
+
+  get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'artists/show'
   end
 
 end
