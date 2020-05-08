fce4302f4c8fc7013c7c307a095c2a4a7c231d91
ksummerill <kaitlynsummerill@gmail.com>
2020-05-07 17:49:20 -0700

    sinatra playlister lab

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,9 @@
+require 'sinatra/base'
+require 'rack-flash'
+
 class SongsController < ApplicationController
+
+  use Rack::Flash
 
   # This should present the user with a list of all songs in the library.
   # Each song should be a clickable link to that particular song's show page.
@@ -20,15 +25,33 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    # binding.pry
     @song = Song.create(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-
     @song.genre_ids = params[:genres]
     @song.save
+
+    flash[:message] = "Successfully created song."
 
     redirect "/songs/#{@song.slug}"
   end
 
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
 
+  # Be able to change everything about a song,
+  # including the genres associated with it and its artist
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.artist = @artist
+    @song.genres = []
+    genres = params[:genres].collect {|id| @song.genres << Genre.find_by_id(id)}
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+
+    redirect "songs/#{@song.slug}"
+  end
 end
