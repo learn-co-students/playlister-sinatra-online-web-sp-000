a41c82324960f73a8bb154bf0523be13ab291e26
mlgvla <mlgerardvla@gmail.com>
2020-05-20 20:09:45 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,5 +25,22 @@ class SongsController < ApplicationController
 
         redirect "songs/#{@song.slug}"
     end
+
+    get '/songs/:slug/edit' do
+        @song = Song.find_by_slug(params[:slug])
+
+        erb :'/songs/edit'
+    end
+
+    patch '/songs/:slug' do
+        # binding.pry
+        @song = Song.find_by_slug(params[:slug])
+        @song.update(params[:song])
+        @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+        @song.genre_ids = params[:genres]
+        @song.save
+
+        redirect "songs/#{@song.slug}"
+    end
     
 end
