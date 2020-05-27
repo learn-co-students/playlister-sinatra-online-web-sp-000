4eab22bd117debc2a007854d156ba406585bc428
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2020-05-27 12:28:11 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,9 +1,12 @@
 require_relative './concerns/slugifiable.rb'
 
 class Artist < ActiveRecord::Base
-    extend Slugifiable
+    extend Slugifiable::ClassMethods
     include Slugifiable::InstanceMethods
 
     has_many :songs
     has_many :genres, through: :songs
+    def self.find_by_slug(b)
+        self.all.find {|a| a.slug == b}
+    end
 end
--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -10,12 +10,14 @@
 # end
 
 module Slugifiable 
-    def self.find_by_slug(a)
-        self.all.find {|a| a.slug == a.downcase}
-    end
-    module InstanceMethods
-      def slug
-        self.name.downcase.gsub(" ", "-")
-      end
+  module ClassMethods
+    def self.find_by_slug(b)
+        self.all.find {|a| a.slug == b}
     end
   end
+  module InstanceMethods
+    def slug
+      self.name.downcase.gsub(" ", "-")
+    end
+  end
+end
