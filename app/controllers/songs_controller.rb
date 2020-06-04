67d83f65415efbca09e7f9e93ca6c50a3ae93dc5
Ronda D Cowen <cowenhome5@gmail.com>
2020-06-04 21:09:58 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,22 @@
+require 'rack-flash'
+class SongsController < ApplicationController
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+
+  end
+  get 'songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
+  
+  get '/songs/:slug/edit' do
+
+  end
+
+
+
+end
