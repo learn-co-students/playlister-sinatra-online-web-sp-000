82c564b3e5f077bc49a424068623284f8f56b8fa
jessicagomesng <jgnpeluso@gmail.com>
2020-06-06 16:50:54 +0100

    Done.

---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,15 @@
+module Slugifiable 
+    module InstanceMethods
+        def slug 
+            self.name.downcase.gsub(" ", "-")
+        end 
+    end
+
+    module ClassMethods 
+        def find_by_slug(slug) 
+            unslugged_name = slug.gsub("-", " ").titlecase
+
+            self.find_by(name: unslugged_name)
+        end 
+    end
+end 
