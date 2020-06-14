431655c04cbfc41f9c7e431e8b54bfd580836af4
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-06-14 03:26:31 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,13 +1,21 @@
 class Artist < ActiveRecord::Base
+  
+  include Slugifiable::InstanceMethods
+  
   has_many :songs
   has_many :genres, through: :songs
+
   
-  def slug
-    self.name.downcase.strip.gsub(' ', '-')
-  end
-  
-  def self.find_by_slug(slug)
-    self.all.detect {|e| e.name = slug.gsub('-',' ').split.collect {|e| e.capitalize}.join(" ")}
-  end
-  
+  extend Slugifiable::ClassMethods
 end
+
+
+
+  
+  # def slug
+  #   self.name.downcase.strip.gsub(' ', '-')
+  # end
+  
+  # def self.find_by_slug(slug)
+  #   self.all.detect {|e| e.name = slug.gsub('-',' ').split.collect {|e| e.capitalize}.join(" ")}
+  # end
