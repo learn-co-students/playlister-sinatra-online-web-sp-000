e05c4d577c0c0e55711920cb369f78b7b35fb59e
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-06-14 03:50:31 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,12 +1,11 @@
 class Artist < ActiveRecord::Base
   
   include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
   
   has_many :songs
   has_many :genres, through: :songs
 
-  
-  extend Slugifiable::ClassMethods
 end
 
 
