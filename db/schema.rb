d5bab5a28093f0e3e75baea84f8ef2300c608d64
cantstopcoding <jonathan_koss@yahoo.com>
2020-06-18 21:11:44 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 2020_06_16_184555) do
   end
 
   create_table "song_genres", force: :cascade do |t|
-    t.integer "song_ids"
-    t.integer "genre_ids"
+    t.integer "song_id"
+    t.integer "genre_id"
   end
 
   create_table "songs", force: :cascade do |t|
