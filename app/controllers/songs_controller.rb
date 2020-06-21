93ecc988aba3ac073ffb5167f580cd5ec014bac6
ArslanInfinite <ali.arslan319@gmail.com>
2020-06-21 15:05:20 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,4 +5,11 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end 
 
+  #new goes before slug
+
+  get '/songs/:slug' do 
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
+  end 
+
 end
