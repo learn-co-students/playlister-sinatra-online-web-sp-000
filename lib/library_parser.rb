1953338f257dc6d9e64d40a3a30061baf00cc149
tbanish <tombanish@gmail.com>
2020-07-08 16:59:39 -0400

    Done.

--- lib/library_parser.rb
+++ lib/library_parser.rb
@@ -16,7 +16,7 @@ class LibraryParser
     artist = artist_match && artist_match[1]
     song   = song_match   && song_match[1]
     genre  = genre_match  && genre_match[1]
-
+    binding.pry
     [artist, song, genre]
   end
 
@@ -34,7 +34,7 @@ class LibraryParser
 
     song.song_genres.build(genre: genre)
     song.artist = artist
-    
+
     song.save
   end
 end
