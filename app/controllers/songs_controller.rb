7fa6d746e6ef8df84baae511fb2c8cc4f707c39e
The-Gentleman <zraja4402@gmail.com>
2020-07-27 11:01:30 -0400

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,28 @@
+require 'rack-flash'
+class SongsController < ApplicationController
+    
+    get '/songs' do 
+        erb :'/songs/index'
+    end 
+
+    get '/songs/new' do 
+        erb :'/songs/new'
+    end 
+
+    get '/songs/:slug' do 
+        erb :'/songs/show'
+    end 
+
+    post '/songs' do
+        @song = Song.create(params[:song])     
+        @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+        @song.genre_ids = params[:genres]
+        @song.save 
+        # binding.pry
+
+        flash[:message] = "Successfully created song."
+        redirect to("/songs/#{@song.slug}")
+    end
+
+
+end
