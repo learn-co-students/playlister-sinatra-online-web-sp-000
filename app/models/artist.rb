d2e2640078153336c7efc283fcc4143fdd6db2ea
SVR <samuel@rourke.tech>
2020-08-16 17:24:19 -0400

    more work on Artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,5 @@
-# can have multiple songs
-# can have multiple genres
 class Artist < ActiveRecord::Base
+    # can have multiple songs
     has_many :songs
-    
-
+    # can have multiple genres
 end
