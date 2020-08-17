8b8e36906a1ee3b230e3b5256dee00512fef17a2
SVR <samuel@rourke.tech>
2020-08-17 14:15:46 -0400

    Seeded Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,6 @@
 # Add seed data here. Seed your database with `rake db:seed`
+require_relative './../config/environment.rb'
+require_relative "./../lib/library_parser.rb"
 
+parser = LibraryParser.new
+parser.call
