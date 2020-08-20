36f556da15cbfac7847b8bb695e9bd1faa8c1cc0
SVR <samuel@rourke.tech>
2020-08-20 17:55:37 -0400

    saving progress

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,6 +22,33 @@ class SongsController < ApplicationController
         redirect "/songs/#{@song.slug}", 303, "Successfully created song."
     end
     
+    get '/songs/:slug/edit' do
+        @song = Song.find_by_slug(params[:slug])
+        erb :edit_song
+    end
+
+    post '/songs/:slug/edit' do
+        @song = Song.find_by_slug(params[:slug])
+        @song.update(
+            name: params[:name],
+            artist: Artist.find_or_create_by(:name => params["Artist Name"])
+        )
+        @song.genres.each do |g|
+            if !!params[:genres].include?(g.id)
+                @song.genres.destory(g)
+            end
+        end
+
+        params[:genres].each do |genre|
+            @song.genres << Genre.find_by_or_create_by(:id, genre)
+        end
+
+        @song.save()
+
+        # @song = Song.find_by_slug(params[:slug])
+        redirect.to "/songs/#{@song.slug}"
+    end
+
     get '/songs/:slug' do
         @song = Song.find_by_slug(params[:slug])
         erb :show_song
