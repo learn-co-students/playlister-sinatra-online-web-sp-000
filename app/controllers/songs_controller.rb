60544d6e40342c262d11b6744c845ede844f82b6
SVR <samuel@rourke.tech>
2020-08-20 22:47:37 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,19 +6,15 @@ class SongsController < ApplicationController
 
     get '/songs/new' do
         @genres = Genre.all
-        # @artists = Artist.all
         erb :new_song
     end
 
     post '/songs/new' do
         @artist = Artist.find_by(:name => params["Artist Name"])
-        
         if @artist.nil?
             @artist = Artist.create(name: params["Artist Name"])
         end
         @song = Song.create( name: params["Name"], genre_ids: params["genres"], :artist => @artist)
-        # binding.pry
-        
         redirect "/songs/#{@song.slug}", 303, "Successfully created song."
     end
     
@@ -29,24 +25,14 @@ class SongsController < ApplicationController
 
     post '/songs/:slug/edit' do
         @song = Song.find_by_slug(params[:slug])
-        @song.update(
-            name: params[:name],
-            artist: Artist.find_or_create_by(:name => params["Artist Name"])
-        )
-        @song.genres.each do |g|
-            if !!params[:genres].include?(g.id)
-                @song.genres.destory(g)
-            end
+        @artist = Artist.find_or_create_by(:name => params["Artist Name"]) 
+        @song.artist = @artist
+        @song.save
+        @song.genres.clear()
+        params[:genres].each do |p|
+            @song.genres << Genre.find_or_create_by(name: p)
         end
-
-        params[:genres].each do |genre|
-            @song.genres << Genre.find_by_or_create_by(:id, genre)
-        end
-
-        @song.save()
-
-        # @song = Song.find_by_slug(params[:slug])
-        redirect.to "/songs/#{@song.slug}"
+        redirect to("/songs/#{@song.slug}")
     end
 
     get '/songs/:slug' do
