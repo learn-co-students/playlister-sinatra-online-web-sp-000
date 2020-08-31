ba3691bf122825035711eb8409fd1ee457bd1865
joseph <josephtkao@gmail.com>
2020-08-31 18:12:14 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'sinatra'
+gem 'sinatra-flash'
 gem 'activerecord', '~> 5.2.3', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 gem 'rake'
@@ -10,6 +11,8 @@ gem 'thin'
 gem 'shotgun'
 gem 'pry'
 gem 'tux'
+gem 'rack-flash3'
+
 
 group :test do
   gem 'launchy'
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,12 @@
 class SongsController < ApplicationController
-    get '/songs' do
+    enable :sessions
+    register Sinatra::Flash
+   
+    get '/bleh' do
+        flash[:message]="Hello?"
+    end
+
+     get '/songs' do
         @songs = Song.all
         erb :"/songs/index"
     end
@@ -7,6 +14,18 @@ class SongsController < ApplicationController
     get '/songs/new' do
         erb :"/songs/new"
     end
+
+    post '/songs' do
+        @song = Song.create(params[:song])
+        @song.artist= Artist.find_or_create_by(:name => params[:artist]["name"])
+        @song.genres << Genre.find(params[:genres])
+        @song.save
+
+        flash[:message] = "Successfully created song."
+        redirect to "songs/#{@song.slug}"
+    end
+
+
     get '/songs/:slug' do
         @song = Song.find_by_slug(params[:slug])
         erb :"/songs/show"
