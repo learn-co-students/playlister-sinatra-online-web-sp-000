84bebaf14ed495bfa6783f785e2af8f8b6d173ab
joseph <josephtkao@gmail.com>
2020-08-31 19:45:29 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ require 'sinatra/flash'
 
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-  register Sinatra::Flash
   enable :sessions
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -31,4 +31,20 @@ class SongsController < ApplicationController
         erb :"/songs/show"
     end
 
+    get '/songs/:slug/edit' do
+        @song = Song.find_by_slug(params[:slug])
+        erb :"/songs/edit"
+    end
+
+    patch 'songs/:slug' do
+        @song = Song.find_by_slug(params[:slug])
+        @song.update(params[:song])
+        @song.save
+
+        flash[:message]="Successfully updated song."
+        redirect to "/songs/#{@song.slug}"
+    
+    end
+
+
 end
