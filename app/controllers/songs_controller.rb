fff40529e9c936188a327a0904bd95b33d7412dd
zacharytq <zacharytq@gmail.com>
2020-09-14 12:20:25 -0400

    finished basic spec

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,13 @@
+class SongsController < ApplicationController
+
+    get '/songs' do
+        @songs = Song.all
+        erb :"/songs/index"
+    end
+
+    get "/songs/:slug" do
+        slug = params[:slug]
+        @song = Song.find_by_slug(slug)
+        erb :'/songs/show'
+    end
+end
