1ab54a252cb9236d3da9c707acb1141c31e86638
zacharytq <zacharytq@gmail.com>
2020-09-14 10:52:52 -0400

    all models passing spec

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # Add seed data here. Seed your database with `rake db:seed`
 nasx = Artist.create(name: "Lil' Nas X")
 bill_strings = Artist.create(name: "Billy Strings")
-rap = Genre.find_by_name("Rap")
-rnb = Genre.find_by_name("R&B")
+rap = Genre.create(name: "Rap")
+rnb = Genre.create(name: "R&B")
 country = Genre.create(name: "Country")
 bluegrass = Genre.create(name: "Bluegrass")
 Song.create(name: "Old Town Road", artist: nasx, genres: [rap, rnb])
