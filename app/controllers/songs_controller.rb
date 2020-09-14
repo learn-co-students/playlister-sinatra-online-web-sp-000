7ddfbe626055fd4b19768f0cad52517d231f64b9
zacharytq <zacharytq@gmail.com>
2020-09-14 14:33:21 -0400

    working on edit ong function

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -34,6 +34,23 @@ class SongsController < ApplicationController
         redirect to "/songs/#{@song.slug}"
     end
 
+    get "/songs/:slug/edit" do
+        @song = Song.find_by_slug(params[:slug])
+        erb :'/songs/edit'
+    end
+
+    patch '/songs/:slug' do
+        song = Song.find_by_slug(params[:slug])
+        song.artist = Artist.find_or_create_by(name: params[:song][:artist])
     
+        genre_selections = params[:song][:genres]
+        genre_selections.each do |genre|
+          song.genres << Genre.find(genre)
+        end
+    
+        song.save
+        flash[:message] = "Successfully updated song."
+        redirect to "/songs/#{song.slug}"
+    end
         
 end
