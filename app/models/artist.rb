055212d71d300fd109e5d0e927f02efaa80aa9d5
reevesalic <reevesalic@gmail.com>
2020-09-28 10:26:42 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,4 @@
-class Artists < ActiveRecord::Base
+class Artist < ActiveRecord::Base
      has_many :songs
      has_many :genres, :through => :songs
    
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,4 @@
-class Genres < ActiveRecord::Base
+class Genre < ActiveRecord::Base
      has_many :song_genres
      has_many :songs, through: :song_genres
      has_many :artists, through: :songs
