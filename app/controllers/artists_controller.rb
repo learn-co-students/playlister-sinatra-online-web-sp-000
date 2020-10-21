745384289e3a2e3e9e9da5d14a76309568d2681e
sparsell <hello@sparsell.com>
2020-10-21 10:47:59 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -11,15 +11,17 @@ class ArtistsController < ApplicationController
         erb :'artists/index'
       end
 
-      get '/artists/:slug' do
-        @artist = Artist.find_by_slug(params[:slug])
-        # binding.pry
-        erb :'artists/show'
-    end
 
       get '/artists/new' do
         erb :'artists/new'
       end
+
+      get '/artists/:slug' do
+        @artist = Artist.find_by_slug(params[:slug])
+        # binding.pry
+        erb :'artists/show'
+      end
+
 
   
 end
