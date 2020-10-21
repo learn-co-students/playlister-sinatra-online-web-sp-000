67dbddbc9009786ba643ff009a7b84704b80e3ef
sparsell <hello@sparsell.com>
2020-10-21 17:57:50 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -44,32 +44,27 @@ class SongsController < ApplicationController
         erb :'songs/show'
       end
 
-      
-      ##############
-
-
       ####### UPDATE
       get '/songs/:slug/edit' do
-        
         @song = Song.find_by_slug(params[:slug])
-        @artists = Artist.all 
-        @songs = Song.all 
+        # @artist = @song.artist 
         @genres = Genre.all 
         erb :'songs/edit'
       end
 
       patch '/songs/:slug' do 
-        # binding.pry
         @song = Song.find_by_slug(params[:slug])
-        
         @song.update(params[:song])
         @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-        @song.genres = params[:genre_ids]
         # binding.pry
+        @song.genres_id = params[:genre_ids]
+        # @song.genres = Genre.find_or_create_by(genre_ids: params[:genre_ids])
+      #  binding.pry
         # params[:genre_ids].each do |genre_id|
-        #     @genres = Genre.find_by(id: genre_id)
+        #     @genres = Genre.find_by(genre_ids: params[genre_ids])
         #     @song.genres << genre
         # end
+        
         @song.save
         flash[:message] = "Successfully updated song."
         
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,7 +3,6 @@ class Artist < ActiveRecord::Base
     include Slugifiable::InstanceMethods
     
     has_many :songs
-    has_many :song_genres
-    has_many :genres, through: :song_genres
+    has_many :genres, through: :songs
     
 end
