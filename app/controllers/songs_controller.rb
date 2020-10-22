e912fdf02ec4f595cf2c07f4d80748fb1fc0b6fd
kkirby16 <kkirby16@my.whitworth.edu>
2020-10-21 17:59:20 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,5 @@
 class SongsController < ApplicationController
+  get "/songs" do
+    @songs = Song.all
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,16 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    self.name.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slug)
+    unslugged_name = slug.split("-").map do |word|
+      word.capitalize
+    end.join(" ")
+
+    self.all.find_by(name: unslugged_name)
+  end
 end
