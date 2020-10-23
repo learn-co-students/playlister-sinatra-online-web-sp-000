e26080b450a56ff06f719d137918243b85ec8b76
codyalvarez <jedi.alvarez805@gmail.com>
2020-10-23 16:59:54 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_200732) do
+ActiveRecord::Schema.define(version: 2020_10_23_205250) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "song_genres", force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "genre_id"
   end
 
   create_table "songs", force: :cascade do |t|
