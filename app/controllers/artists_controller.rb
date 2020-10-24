e3bf82a1d03f83b8555a433ae12ffb17d7492bda
codyalvarez <jedi.alvarez805@gmail.com>
2020-10-24 10:59:34 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,33 +1,7 @@
 class ArtistsController < ApplicationController
-    
-    # get '/songs/new' do
-    # end 
-    
-    # get '/songs' do
-    #     @songs = Song.all        
-    #     erb :'/songs/show/' 
-    # end
 
-    # get '/songs/:slug/edit' do
-    # end
-
-    # get '/genres' do
-    #     @genres = Genre.all
-    #     erb :'/genres/show'
-    # end
-
-    # get '/artists' do
-    #     @artists = Artist.all
-    #     erb :'/artists/show'
-    # end
-
-    # get '/songs/:slug' do        
-    # end
-
-    # get '/artists/:slug' do   
-    # end
-
-    # get '/genres/:slug' do     
-    # end
-  
+  get '/artists' do
+    @artists = Artist.all 
+    erb :'artists/index'
+  end 
 end
