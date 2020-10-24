594cb41488ac455b5f1bb21b6565ec2688e6a778
codyalvarez <jedi.alvarez805@gmail.com>
2020-10-24 16:35:23 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,18 +1,17 @@
 class SongsController < ApplicationController
 
   get '/songs' do
-      @songs = Song.all 
-      erb :'/songs/index'
+    @songs = Song.all 
+    erb :'/songs/index'
   end
 
   get '/songs/new' do
     erb :'/songs/new'
-    redirect to '/songs/:slug'
   end
 
   get '/songs/:slug' do
-      @song = Song.find_by_slug(params[:slug])
-      erb :'songs/show'
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
   end
 
   post '/songs' do
@@ -20,24 +19,24 @@ class SongsController < ApplicationController
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
     @song.genre_ids = params[:genres]
     @song.save
-
+  
     erb :'songs/show', locals: {message: "Successfully created song."}
   end
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
-
+  
     erb :'songs/edit'
   end
-
+  
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-
+  
     @song.update(params[:song])
-
+  
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.save
-
+  
     erb :'songs/show', locals: {message: "Song successfully updated."}
   end
     
