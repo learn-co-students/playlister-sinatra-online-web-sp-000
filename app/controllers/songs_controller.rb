d620b8cbd0ca5cff5804c2fb415acd6d1f13f8ae
markdrehmann <markdrehmann@gmail.com>
2020-11-05 23:42:27 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,9 @@
+require 'sinatra/base'
+require 'rack-flash'
+
 class SongsController < ApplicationController
+    enable :sessions
+    use Rack::Flash
 
     get '/songs' do
         @songs = Song.all
@@ -13,17 +18,15 @@ class SongsController < ApplicationController
     post '/songs' do
         @song = Song.create(name: params[:song][:name])
         artist = Artist.find_or_create_by(name: params[:artist][:name])
-        binding.pry
-
         genre_ids = params[:song][:genres]
         genre_ids.each do |id|
-            # genre = sql statement to return Genre object?
+            genre = Genre.find(id)
             @song.genres << genre
         end
-
         @song.artist = artist
         @song.save
 
+        flash[:message] = "Successfully created song."
         redirect "/songs/#{@song.slug}"
     end
 
@@ -32,4 +35,17 @@ class SongsController < ApplicationController
         erb :'/songs/show'
     end
 
+    get '/songs/:slug/edit' do
+        slug = params[:slug]
+        @song = Song.find_by_slug(slug)
+        @genres = Genre.all
+        erb :'/songs/edit'
+    end
+
+    patch '/songs/:slug' do
+
+
+        flash[:message] = "Successfully updated song."
+        redirect to "songs/#{song.slug}"
+    end
 end
