fc4aa0f7ecc2003bd29edb8ddbbc7dd86732355a
William Pannell <lpann5@gmail.com>
2020-11-24 20:35:48 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,7 +7,7 @@ class SongsController < ApplicationController
     @songs = Song.all
     erb :'/songs/index'
   end
-
+  
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
