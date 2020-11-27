cfac42c15e5a77c04258c0b78156b378d92e43ef
William Pannell <lpann5@gmail.com>
2020-11-27 13:35:15 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,10 @@ class ApplicationController < Sinatra::Base
     @songs = Song.all
     erb :'/songs/index'
   end
+  
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
+  end
+
 end
