208d984d021f07f73bf1a5c9feba3ff8e512d972
Chandra Lama <iamme24cl@gmail.com>
2020-12-09 14:58:23 -0800

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,17 +1,10 @@
-class Song < ActiveRecord::Base
-  # inlcude Slugifiable::IntanceMethods
-  # extend Slugifiable::ClassMethods
+require_relative './concerns/slugifiable'
 
+class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
 
-
-  def slug
-    name.downcase.gsub(" ", "-")
-  end
-
-  def self.find_by_slug(slug)
-    Song.all.find{|song| song.slug == slug}
-  end
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
 end
