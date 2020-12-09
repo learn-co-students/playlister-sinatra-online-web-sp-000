358bd3ed3e43c5e2d53a95cd0e6607989834762f
Chandra Lama <iamme24cl@gmail.com>
2020-12-09 02:03:11 -0800

    Done.

---
+++ app/models/artist.rb
@@ -0,0 +1,16 @@
+class Artist < ActiveRecord::Base
+  # inlcude Slugifiable::IntanceMethods
+  # extend Slugifiable::ClassMethods
+
+  has_many :songs
+  has_many :genres, through: :songs
+
+
+  def slug
+    name.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    Artist.all.find{|artist| artist.slug == slug}
+  end
+end
