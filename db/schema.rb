cd554187a84f6934ea4bd5a227a52ff69bdc957c
JustinBreneman <Justin.Breneman@yahoo.com>
2020-12-17 20:08:57 -0800

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_12_18_035234) do
     t.string "name"
   end
 
-  create_table "song_genre", force: :cascade do |t|
+  create_table "song_genres", force: :cascade do |t|
     t.integer "genre_id"
     t.integer "song_id"
   end
