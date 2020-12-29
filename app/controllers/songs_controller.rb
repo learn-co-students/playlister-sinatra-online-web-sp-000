1d8f40d5d694e6595f144d37f7f718e3cdac019c
Catherine Gloria <catherinegloria@gmail.com>
2020-12-29 07:48:54 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,7 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+  use Rack::Flash
 
   get '/songs' do
     @songs = Song.all
@@ -15,6 +18,7 @@ class SongsController < ApplicationController
     @song.genre_ids = params[:genres]
     @song.save
 
+    flash[:message] = "Successfully created song."
     redirect to "/songs/#{@song.slug}"
   end
 
@@ -35,6 +39,7 @@ class SongsController < ApplicationController
     @song.genre_ids = params[:genres]
     @song.save
 
+    flash[:message] = "Successfully updated song."
     redirect to "/songs/#{@song.slug}"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 # Add seed data here. Seed your database with `rake db:seed`
-@artist = Artist.create(:name => "Taylor Swift")
-blank_space =  Song.create(:name => "Blank Space", :artist => @artist)
-pop = Genre.create(:name => "Pop")
-blank_space.genre_ids = pop.id
-
-@artist = Artist.create(:name => "Justin Timberlake")
-cry_me_a_river =  Song.create(:name => "Cry Me A River", :artist => @artist)
+#@artist = Artist.create(:name => "Taylor Swift")
+#blank_space =  Song.create(:name => "Blank Space", :artist => @artist)
 #pop = Genre.create(:name => "Pop")
-cry_me_a_river.genre_ids = pop.id
+#blank_space.genre_ids = pop.id
 
-@artist = Artist.create(:name => "TLC")
-waterfalls =  Song.create(:name => "Waterfalls", :artist => @artist)
-rnb = Genre.create(:name => "RnB")
-waterfalls.genre_ids = rnb.id
+#@artist = Artist.create(:name => "Justin Timberlake")
+#cry_me_a_river =  Song.create(:name => "Cry Me A River", :artist => @artist)
+#pop = Genre.create(:name => "Pop")
+#cry_me_a_river.genre_ids = pop.id
 
-#use Library Parser in seeds
+#@artist = Artist.create(:name => "TLC")
+#waterfalls =  Song.create(:name => "Waterfalls", :artist => @artist)
+#rnb = Genre.create(:name => "RnB")
+#waterfalls.genre_ids = rnb.id
+
+LibraryParser.parse
