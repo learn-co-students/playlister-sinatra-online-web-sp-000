1acca4ca7aea7366d1d90f52e8c21257e732a77c
Benjamin Cheng <bencheng200@gmail.com>
2021-01-12 22:58:22 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,13 +2,6 @@ class Artist < ActiveRecord::Base
 has_many :songs
 has_many :genres, :through => :songs
 
-# We are repeating a ton of code here. These two methods could go into a module.
-
-def slug
-  name.downcase.gsub(" ","-")
-end
-
-def self.find_by_slug(slug)
-  Artist.all.find{|artist| artist.slug == slug}
-end
+extend Slugifiable::InstanceMethods
+include Slugifiable::ClassMethods
 end
