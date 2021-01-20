15215f1cf40ad32d45add33364cb3a4cd56da286
github username <github email address>
2021-01-19 20:51:15 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,10 +9,24 @@ class SongsController < ApplicationController
         erb :'songs/new'
     end
 
+    post '/songs' do
+        #binding.pry
+        song = Song.create(params[:song])
+
+        if !params[:artist][:name].empty?
+            song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+            song.save
+        end
+
+    end
+
+
     get '/songs/:slug' do
         @song = Song.find_by_slug(params[:slug])
         erb :'songs/show'
     end
+
+    
 
     
 end
