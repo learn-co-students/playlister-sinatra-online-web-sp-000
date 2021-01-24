b3e9a5e2f5ba175708ef3461e6c84d7ca1d95abb
David Strauch <dmaster18@gmail.com>
2021-01-24 23:49:04 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,10 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  enable :sessions
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
     erb :index
   end
-  
-  get '/songs' do 
-  
-  end
-  
-  get '/artists' do 
-  
-  end
-  
-  get '/songs/:slug' do 
-  
-  
-  end
-  
-  get '/artists/:slug' do 
-    
-  end
-  
-  
-  get '/genres/:slug' do 
-  
-  end
-  
 end
