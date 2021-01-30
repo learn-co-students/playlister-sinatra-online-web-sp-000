c395a3796163368f789b00964ca145e8fd711ab3
deliaconstantino <dconstan92@gmail.com>
2021-01-30 17:56:36 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -35,11 +35,12 @@ class SongsController < ApplicationController
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
-    @genres = Genre.all
+    # @genres = Genre.all
     erb :'/songs/edit'
   end
 
   patch '/songs/:slug' do
+    # binding.pry
     @song = Song.find_by_slug(params[:slug])
     @song.genres = []
 
