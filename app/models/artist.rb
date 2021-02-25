b798ee3aa66bf24151fbd70213c223fc105b662d
Christerle Nounoute <christerlesn@gmail.com>
2021-02-25 01:51:10 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,6 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
-  extend Slugifable::ClassMethods
-  include Slugifable::InstanceMethods
+  # extend Slugifable::ClassMethods
+  # include Slugifable::InstanceMethods
 end
