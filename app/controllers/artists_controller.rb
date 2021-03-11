04983230a0c1f8e75d646a14aa021be5258aadf2
danny galimi <dannygalimi13@aim.com>
2021-03-11 21:39:13 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,5 +4,10 @@ get '/artists' do
     @artists = Artist.all
     erb :'/artists/index'
   end
+  
+  get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'artists/show'
+  end
 
 end
