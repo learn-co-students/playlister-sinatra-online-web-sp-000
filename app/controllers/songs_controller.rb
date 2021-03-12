b02915c28e5d617c561ad1bd0e34184b90e358fa
danny galimi <dannygalimi13@aim.com>
2021-03-12 18:45:11 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,7 +7,7 @@ class SongsController < ApplicationController
     @songs = Song.all
     erb :'/songs/index'
   end
-  
+
   get '/songs/new' do
     erb :'/songs/new'
   end
@@ -16,7 +16,7 @@ class SongsController < ApplicationController
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
   end
-  
+
   post '/songs' do
     @song = Song.create(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
@@ -26,7 +26,22 @@ class SongsController < ApplicationController
     flash[:message] = "Successfully created song."
     redirect("/songs/#{@song.slug}")
   end
-  
+
+  get '/songs/:slug/edit' do
+      @song = Song.find_by_slug(params[:slug])
+      erb :'songs/edit'
+    end
+
+    patch '/songs/:slug' do
+      @song = Song.find_by_slug(params[:slug])
+      @song.update(params[:song])
+      @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+      @song.genre_ids = params[:genres]
+      @song.save
+
+      flash[:message] = "Successfully updated song."
+      redirect("/songs/#{@song.slug}")
+    end
+
 
 end
-
--- config.ru
+++ config.ru
@@ -6,9 +6,13 @@ begin
   use Rack::MethodOverride
   run ApplicationController
   use SongsController
-  use ArtistsController
-  use GenresController
 rescue ActiveRecord::PendingMigrationError => err
   STDERR.puts err
   exit 1
 end
+
+use Rack::MethodOverride  
+use GenresController
+ use ArtistsController
+ use SongsController
+ run ApplicationController 
