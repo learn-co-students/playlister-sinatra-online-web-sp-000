8b671d558e1e5db715e6d9a215c046015813d769
GoldenPavilion <rcorystine@gmail.com>
2021-04-02 12:25:22 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,68 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+    enable :sessions
+    use Rack::Flash
+    
+    get '/songs' do 
+        @songs = Song.all
+        erb :'/songs/index'
+    end
+
+    get '/songs/new' do
+        @genres = Genre.all
+        erb :'/songs/new'
+    end
+
+    post '/songs' do
+        @song = Song.create(name: params[:name])
+        @song.artist = Artist.find_or_create_by(name: params[:artist])
+        genres = params[:song][:genres]
+        genres.each do |genre|
+            @song.genres << Genre.find(genre)
+        end 
+        
+        @song.save
+        flash[:message] = "Successfully created song."
+     
+        redirect to "/songs/#{@song.slug}"
+    end
+
+    get '/songs/:slug/edit' do
+        @song = Song.find_by_slug(params[:slug])
+        @genres = Genre.all
+        erb :'songs/edit'
+    end
+
+    patch '/songs/:slug' do
+        @song = Song.find_by_slug(params[:slug])
+        @song.name = params[:song][:name]
+
+        artist_name = params[:song][:artist]
+        if Artist.find_by(:name => artist_name)
+            if song.artist.name != artist_name
+                song.artist = Artist.find_by(:name => artist_name)
+            end
+        else
+            @song.artist = Artist.create(:name => artist_name)
+        end
+
+        if song.genres
+            song.genres.clear
+        end
+        
+        genres = params[:song][:genres]
+        genres.each do |genre|
+            song.genres << Genre.find(genre)
+        end
+        song.save
+        
+        redirect to "/songs/#{@song.slug}"
+    end
+
+    get '/songs/:slug' do
+        @song = Song.find_by_slug(params[:slug])
+        erb :'/songs/show'
+    end
 
 end
--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -7,4 +7,19 @@ module Slugifiable
         end
     end
 
+    module ClassMethods
+    
+        def find_by_slug(slug)
+            found_song = ''
+            self.all.each do |song|
+                if song.name.parameterize == slug 
+                    found_song = song
+                end
+            end
+
+            found_song
+        end
+
+    end
+
 end
