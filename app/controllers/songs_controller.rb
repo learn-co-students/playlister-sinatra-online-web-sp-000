e4f07c47080e53ad96500320dddccc96b712a6d3
Jouse then <jthen93@gmail.com>
2021-05-05 20:19:05 +0000

    finished models

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,18 @@
 class SongsController < ApplicationController
+  
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index' 
+  end
+  
+  get '/songs/new' do
+    
+  end
+  
+  get '/songs/:slug' do
+    @song = Song.find(params[:id])
+    erb :'/songs/show' 
+  end
+  
+  
 end
