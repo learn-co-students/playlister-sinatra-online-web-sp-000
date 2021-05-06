d9f6ae6688f5bf8fcab23c01ff7031a29c61272b
Jouse then <jthen93@gmail.com>
2021-05-06 20:06:20 +0000

    New and show working

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,17 +6,42 @@ class SongsController < ApplicationController
   end
   
   get '/songs/new' do
-    
+    @genres = Genre.all
+    erb :'/songs/new'
   end
   
   get '/songs/:slug' do
     
     @song = Song.find_by_slug(params[:slug])
-    binding.pry
+    
     @artist = Artist.find(@song.artist_id)
-    song_genre = SongGenre.find_by(song_id: @song.id)
-    @genre = Genre.find(song_genre.genre_id)
+   
+    song_genres = SongGenre.where("song_id = ?", @song.id)
+    
+    @genres = song_genres.collect do |g|
+      Genre.find(g.genre_id)
+    end
+    
     erb :'/songs/show' 
+  end
+  
+  post '/songs' do 
+    
+    # See if artist already exists
+    @artist = Artist.find_by(name: params[:artist][:name])
+    
+    if !@artist
+      @artist = Artist.create(params[:artist])
+    end
+    
+    @song = Song.create(name: params[:song][:name], artist_id: @artist.id)
+    
+    params[:genres].each do |genre|
+      SongGenre.create(song_id: @song.id, genre_id: genre)
+    end
+    
+    redirect to "songs/#{@song.slug}"
+  
   end
   
   
