02e6a8a7646ea7905ca0ad9ae337562f6fd58d9d
Michael Dobak <mdobak44@icloud.com>
2021-05-19 14:24:04 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,13 @@
 class ArtistsController < ApplicationController
+
+  get '/artists' do
+    @artists = Artist.all.sort_by {|artist| artist.name }
+    erb :'/artists/index'
+  end
+
+  get '/artists/:slug' do
+    @artists = Artist.find_by_slug(params[:slug])
+    erb :'/artists/show'
+  end
+
 end
